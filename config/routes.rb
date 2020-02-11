e9e66fe79d07ef84219057a6de367250af2271bd
Michael Longerich <mlongerich@gmail.com>
2020-02-11 15:49:37 +0700

    Adds custom routes for portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :portfolios
   resources :blogs
+  resources :portfolios, except: [:show]
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
