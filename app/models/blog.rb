5c9ceafeb38d2fa6dd4c5ca050f6ca1a03ac727c
Michael Longerich <mlongerich@gmail.com>
2020-02-12 10:24:36 +0700

    Adds status to Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+# Model for Blogs
 class Blog < ApplicationRecord
+  enum status: { draft: 0, published: 1 }
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
